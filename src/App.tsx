d4262b64e849d39f2d9b2d1226d07d9f74ee6d63
Braulin Pires <braulinpires@hotmail.com>
2025-07-22 11:44:30 +0100

    set up tailwind environnemt

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="h-screen w-full bg-blue-200 items-center justify-center flex flex-col">
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
-    </>
+    </div>
   );
 }
 
